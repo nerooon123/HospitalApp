a343f0089c40e28be39381db0d826fd56ec8116c
nerooon123 <keleron123@gmail.com>
2023-03-22 12:28:46 +0300

    Добавьте файлы проекта.

---
+++ HospitalApp/View/Pages/MainWindowPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HospitalApp.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindowPage.xaml
+    /// </summary>
+    public partial class MainWindowPage : Page
+    {
+        public MainWindowPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Authorization_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Registration_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new RegistrationPage());
+        }
+    }
+}
